0492e5f2c1ee679dd8e448001c55ba7bc0a1ec77
Aniket Gupta <aniketgupta.org@gmail.com>
2025-02-12 03:37:00 +0530

    update fix

--- src/data/movie.js
+++ src/data/movie.js
@@ -2,18 +2,18 @@ import axios from "axios"
 
 const API_BASE_URL=process.env.NEXT_PUBLIC_API_URL
 const TOKEN=process.env.NEXT_PUBLIC_ACCESS_TOKEN
-const header =  {  
-    accept: 'application/json',
-    Authorization: `Bearer ${TOKEN}`,
-    api_key: process.env.NEXT_PUBLIC_API_KEY
-}
+const header= {
+    "Accept": "application/json",
+    "Authorization": `Bearer ${TOKEN}`,
+    "api_key": process.env.NEXT_PUBLIC_API_KEY,  
+  }
 
 export const getPopularMovies = async()=>{
     try {
         const res = await axios.get(`${API_BASE_URL}/popular?language=en-US&page=1`,{
             headers:header
         })
-        console.log({pop:res.data.results})
+        // console.log({pop:res.data.results})
         return res.data.results
         
     } catch (error) {
@@ -25,7 +25,7 @@ export const getNowPlayingMovies = async()=>{
         const res = await axios.get(`${API_BASE_URL}/now_playing?language=en-US&page=1`,{
             headers:header
         })
-        console.log({lat:res.data.results})
+        // console.log({lat:res.data.results})
 
         return res.data.results
         
@@ -38,7 +38,7 @@ export const getTopRatedMovies = async()=>{
         const res = await axios.get(`${API_BASE_URL}/top_rated?language=en-US&page=1`,{
             headers:header
         })
-        console.log({top:res.data.results})
+        // console.log({top:res.data.results})
 
         return res.data.results
         
@@ -51,7 +51,7 @@ export const getUpComingMovies = async()=>{
         const res = await axios.get(`${API_BASE_URL}/upcoming?language=en-US&page=1`,{
             headers:header
         })
-        console.log({up:res.data.results})
+        // console.log({up:res.data.results})
 
         return res.data.results
         
@@ -66,7 +66,7 @@ export const searchMovies = async(query)=>{
         const res = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,{
             headers:header
         })
-        console.log({search:res.data.results})
+        // console.log({search:res.data.results})
 
         return res.data.results
         
